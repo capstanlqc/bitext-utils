657d8c1d2aee08747aef2f3a8f89806b47d1e791
msoutopico <m.soutopico@gmail.com>
2024-02-02 20:26:16 +0100

    Added a couple of functions to write reports

--- code/find_spacing_diff.py
+++ code/find_spacing_diff.py
@@ -2,30 +2,74 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# run as 
+# python code/find_spacing_diff.py --path /path/to/omegat_project
+# or 
+# python code/find_spacing_diff.py --fix y --path /path/to/omegat_project
+# (to have it fixed)
+
+import argparse
+import json
 import fnmatch
 import os, sys
 import zipfile
 from lxml import etree
 import regex as re
-
-# arguments
-omtprj_path = "/home/souto/Repos/capstanlqc/bitext-utils/repos/pisa_2025ft_translation_hr-HR_verification-review"
-tm_dpath = f"{omtprj_path}/tm"
-locale = "hr-HR" # get it from params or extract it from path or project settings's target_lang
-
-
-# functions 
-def get_target_lang():
-    pass
+from rich import print
+import pandas as pd
+# https://pypi.org/project/prettyformatter/ 
+# https://stackoverflow.com/questions/3229419/how-to-pretty-print-nested-dictionaries
 
 
-def get_translations(fpath):
+# ############# PROGRAM DESCRIPTION ###########################################
+
+text = "Find missing no-break spaces, optionally restores them"
+parser = argparse.ArgumentParser(description=text)
+parser.add_argument("-V", "--version", help="show program version", action="store_true")
+parser.add_argument("-p", "--path", help="specify path to the local clone of the repo hosting the omegat team project")
+parser.add_argument("-f", "--fix", help="specify whether no-break spaces should be restored or not")
+args = parser.parse_args()
+
+version_text = "OmegaT git project manager 1.0"
+if args.version:
+    print(version_text)
+    sys.exit()
+
+fix = args.fix.strip() if args.fix else None
+
+if args.path:
+     omtprj_path = args.path.strip()
+else:
+     print("No 'path' argument has been provided. Run this script with `--help` for details.")
+     sys.exit()
+
+# ############# FUNCTIONS ###########################################
+
+def get_files_by_text(text, instances):
+    return [file for file, translations in instances.items() if text in translations]
+
+
+def get_target_lang(omtprj_path):
+    omegat_project_fpath = os.path.join(omtprj_path, "omegat.project")
+    # pattern = '(?<=TARGET_LANG ")[a-z]+(?=-[^"]+")'
+    pattern = '(?<=TARGET_LANG ")[a-z]+-[^"]+'
+
+    with open(omegat_project_fpath, 'r') as f:
+        omegat_project = f.read()
+    
+    matches = re.findall(pattern, omegat_project)
+    # print("looking for the target language subtag")
+    return matches[0]
+
+
+def get_translations(fpath, locale):
     target_tuvs = []
     parser = etree.XMLParser(resolve_entities=False)
     # doc = etree.parse(fpath, parser)
     with open(fpath, 'r') as f:
         xml = f.read()
     doc = etree.fromstring(xml.encode('utf-8'))
+    
     exprs = [f"//tuv[@xml:lang='{locale}']/seg/text()", f"//tuv[@lang='{locale}']/seg/text()"]
     for expr in exprs:
         target_tuvs.extend(get_nodes(doc, expr))
@@ -41,8 +85,8 @@ def get_nodes(doc, expr):
         return None
 
 
-# find TMs
 def find_files_in_path(tm_dpath):
+    """ Find TMs """
     matches = []
     for root, dirnames, filenames in os.walk(tm_dpath):
         # if root.split("/")[-1] == "prev": # trend
@@ -52,8 +96,8 @@ def find_files_in_path(tm_dpath):
     return matches
 
 
-# unzip TMs
 def unzip_zipped_files(matches):
+    """ Unzip TMs """
     for fpath in matches:
         if fpath.endswith(".tmx.zip"):
             parent_dpath = os.path.dirname(fpath)
@@ -62,13 +106,13 @@ def unzip_zipped_files(matches):
     return True
 
 
-# get text nodes
-def get_translations_per_file(matches):
+def get_translations_per_file(matches, target_lang):
+    """ get text nodes """
     translations_per_file = {}
     for fpath in matches:
         if fpath.endswith(".tmx"):
             fname = os.path.basename(fpath)
-            translations_per_file[fname] = get_translations(fpath)
+            translations_per_file[fname] = get_translations(fpath, target_lang)
     
     return translations_per_file
 
@@ -97,34 +141,102 @@ def find_pattern(translations_per_file, pattern, regex=False):
             # if found without nbsp, report it (file, segment)
 
 
+def edit_file(fpath, replacement_map, locale):
 
-# logic
+    exprs = [f"//tuv[@xml:lang='{locale}']/seg", f"//tuv[@lang='{locale}']/seg"]
+
+    parser = etree.XMLParser(resolve_entities=False)
+    doc = etree.parse(fpath, parser)
+
+    for expr in exprs:
+        if doc.xpath(expr):
+            for seg in doc.xpath(expr):
+                if seg.text in replacement_map:
+                    print(f"Correcting '{seg.text}' to 'replacement_map[seg.text]")
+                    seg.text = replacement_map[seg.text]
+                    
+
+    # etree.indent(common_repo, space=determine_white_space(omegat_project_fpath), level=3)
+    doc.write(fpath, encoding='UTF-8', pretty_print=True, standalone=True)
+
+
+
+def save_report_replacements(replacement_map, instances, omtprj, rpt_suffix):
+    
+    rows = [(key, value, ", ".join(get_files_by_text(value, instances)) )
+        for (key, value) in replacement_map.items()]
+
+    # create a df from the list of tuples
+    df = pd.DataFrame(rows, columns=['Transferred', 'Original trend', 'File'])
+
+    # export
+    df.to_excel(f"{omtprj}_{rpt_suffix}.xlsx", index=False)
+
+
+def save_report_instances(instances, omtprj, rpt_suffix):
+    
+    # create a list of tuples in the desired format
+    rows = [(key, value) for key, values in instances.items() for value in values]
+
+    # create a df from the list of tuples
+    df = pd.DataFrame(rows, columns=['File', 'Text'])
+
+    # export
+    df.to_excel(f"{omtprj}_{rpt_suffix}.xlsx", index=False)
+
+
+
 if __name__ == "__main__":
 
+    target_lang = get_target_lang(omtprj_path)
+    tm_dpath = os.path.join(omtprj_path, "tm")
+    omtprj = os.path.basename(omtprj_path.rstrip("/")).lstrip("pisa_2025ft_translation_")
+    
     tm_files = find_files_in_path(tm_dpath)
     # unzip_zipped_files(tm_files)
-    translations_per_file = get_translations_per_file(tm_files)
+    translations_per_file = get_translations_per_file(tm_files, target_lang)
 
-
+    # character to be found
     substr = "\u00A0"
-    # text = "15 °C"
-    # x = text.encode("unicode_escape")
-    # print(x)
-    # if pattern in text:
-    #     print("found")
 
-
+    working_tm_fpath = os.path.join(omtprj_path, "omegat", "project_save.tmx")
     instances = find_pattern(translations_per_file, substr, False)
-    working_translations = get_translations(os.path.join(omtprj_path, "omegat", "project_save.tmx"))
+
+    working_translations = get_translations(working_tm_fpath, target_lang)
+    replacement_map = {}
 
     for file, translations in instances.items():
         for xlat in translations:
+            # replace the non-breaking space with a normal space to find 
             if xlat.replace(substr, ' ') in working_translations:
-                print(f"Found xlat '{xlat}'")
-
+                replacement_map[xlat.replace(substr, ' ')] = xlat
+                # print(f"Found xlat '{xlat}'")
+    
+    print("\nMatches in TMs containing no-break space(s):")
     print(instances)
-    print()
-    print(working_translations)
+
+    print("\nNecessary corrections:")
+    print(replacement_map)
+
+    if fix and fix.lower().startswith('y'):
+        print("\nRestoring nbsp's in working TM")
+        edit_file(working_tm_fpath, replacement_map, target_lang)
+
+    save_report_instances(instances, omtprj, "nbsp_in_trend")
+    save_report_replacements(replacement_map, instances, omtprj, "replacements")
 
 
-    print("done")
+    print("\ndone")
+
+# caveats: 
+# this assumes that if any no-breaking spaces are found in the trend translation, 
+# all of them have been stripped in the new translation. in other words, it won't flag 
+# mismatches if one nbsp is missed but another one is there (future improvement if needed)
+
+# todo: 
+# perhaps: restrict what files function find_files_in_path finds, to use only trend TMs (not sure
+# this is necessary, it depends on what TMs are added to the trend trasnfer projects) and 
+# to use only the original TMs files, not the ones that Kos created to increase leverage
+
+# todo:
+# fine tune where reports are written
